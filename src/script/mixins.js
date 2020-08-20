70f63c16db44d88ee83f276dae7bdda621fef931
zjkblade <1620662089@qq.com>
2020-08-21 00:22:16 +0800

    整合mixins

--- src/script/mixins.js
+++ src/script/mixins.js
@@ -1,52 +1,124 @@
 var pagination = {
-    data    : function () {
-        return {
-            isLoading: false,
-            hasMore  : true,
-            current  : 0,
-            pageSize : 10,
-            records  : [],
-        }
-    },
-    computed: {
-        hasData: function () {
-            return this.records.length > 0;
-        }
-    },
-    methods : {
-        initPageOptions  : function () {
-            this.current = 0;
-            this.hasMore = true;
-            this.records = [];
-        },
-        getPageOptions   : function (options) {
-            var pager = {
-                current : ++this.current,
-                pageSize: this.pageSize,
-            };
-            return Object.assign({}, pager, options);
-        },
-        loadRecordPromise: function (callback) {
-            var $this = this;
-            if (!$this.isLoading && $this.hasMore) {
-                $this.isLoading = true;
-                callback(
-                    function (options) {
-                        var records = [], pages = 0;
-                        if (options) {
-                            records = options.records;
-                            pages = options.pages;
-                        }
-                        $this.records = $this.records.concat(records);
-                        $this.hasMore = $this.current < pages;
-                        $this.isLoading = false;
-                    },
-                    function () {
-                        $this.hasMore = false;
-                        $this.isLoading = false;
-                    }
-                );
-            }
-        },
+  data: function () {
+    return {
+      isLoading: false,
+      hasMore: true,
+      current: 0,
+      pageSize: 10,
+      records: [],
     }
+  },
+  computed: {
+    hasData: function () {
+      return this.records.length > 0;
+    }
+  },
+  methods: {
+    initPageOptions: function () {
+      this.current = 0;
+      this.hasMore = true;
+      this.records = [];
+    },
+    getPageOptions: function (options) {
+      var pager = {
+        current: ++this.current,
+        pageSize: this.pageSize,
+      };
+      return Object.assign({}, pager, options);
+    },
+    loadRecordPromise: function (callback) {
+      var $this = this;
+      if (!$this.isLoading && $this.hasMore) {
+        $this.isLoading = true;
+        callback(
+          function (options) {
+            var records = [], pages = 0;
+            if (options) {
+              records = options.records;
+              pages = options.pages;
+            }
+            $this.records = $this.records.concat(records);
+            $this.hasMore = $this.current < pages;
+            $this.isLoading = false;
+          },
+          function () {
+            $this.hasMore = false;
+            $this.isLoading = false;
+          }
+        );
+      }
+    },
+  }
 };
+var winpage = {
+  el: '#app',
+  data: function () {
+    return {
+      name: '',
+      frm: '',
+    };
+  },
+  mounted: function () {
+    var $this = this;
+    util.app.ready(function () {
+      util.app.fixStatusBar('header');
+      $this.name = util.app.param('name');
+      $this.listen();
+      $this.init();
+      $this.openFrame();
+    });
+  },
+  methods: {
+    listen: function () {
+      var $this = this;
+      util.app.listen('keyback', function () {
+        $this.close();
+      });
+      util.app.listen('viewappear', function () {
+      });
+    },
+    openFrame: function () {
+      var $this = this;
+      var headerHeight = $api.offset($api.byId("header")).h;
+      util.app.openFrame($this.name, $this.frm, {y: headerHeight}, {id: $this.id});
+    },
+    close: function () {
+      var $this = this;
+      util.app.closeWindow($this.name);
+    },
+    init: function () {
+      // 空实现，有需要页面里重写
+      var $this = this;
+    },
+  }
+}
+var frmpage = {
+  el: '#app',
+  data: function () {
+    return {
+      name: ''
+    };
+  },
+  mounted: function () {
+    var $this = this;
+    util.app.ready(function () {
+      $this.name = util.app.param('name');
+      $this.listen();
+      $this.init();
+    });
+  },
+  methods: {
+    close: function () {
+      var $this = this;
+      util.app.closeWindow($this.name);
+    },
+    listen: function () {
+      // 空实现，有需要页面里重写
+      var $this = this;
+    },
+    init: function () {
+      // 空实现，有需要页面里重写
+      var $this = this;
+    },
+  }
+}
